85940f101db7b36f9b94c70525b402505c8738d2
tmdrbpp123 <tmdrbpp123@gbit.kr>
2024-02-28 08:51:11 +0900

    decembaek 2024 02 28

---
+++ decembaek/2024_02_28/Lv.0/문자열을_정수로_변환하기.py
@@ -0,0 +1,21 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/181848
+
+프로그래머스 Lv.0
+
+제목 : 문자열을 정수로 변환하기
+"""
+
+
+# 풀이 시작
+def solution(n_str):
+    answer = 0
+    return answer
+
+
+# decembaek 풀이
+def solution(n_str):
+    return int(n_str)
+
+
+# 다른사람 풀이
