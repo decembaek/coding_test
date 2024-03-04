f26ab41e0e3ada0b9cd65c2922fbab241796fe90
yeah <yeahyep04@gmail.com>
2024-03-04 21:23:21 +0900

    24-03-04

---
+++ yeah/24.03.04.(월)/lv0배열 두 배 만들기.py
@@ -0,0 +1,25 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/120809
+문제 설명
+정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+-10,000 ≤ numbers의 원소 ≤ 10,000
+1 ≤ numbers의 길이 ≤ 1,000
+입출력 예
+numbers	result
+[1, 2, 3, 4, 5]	[2, 4, 6, 8, 10]
+[1, 2, 100, -99, 1, 2, 3]	[2, 4, 200, -198, 2, 4, 6]
+입출력 예 설명
+입출력 예 #1
+
+[1, 2, 3, 4, 5]의 각 원소에 두배를 한 배열 [2, 4, 6, 8, 10]을 return합니다.
+입출력 예 #2
+
+[1, 2, 100, -99, 1, 2, 3]의 각 원소에 두배를 한 배열 [2, 4, 200, -198, 2, 4, 6]을 return합니다.
+
+"""
+def solution(numbers):
+    return [ numbers[i] * 2 for i in range(len(numbers))]
+
+
