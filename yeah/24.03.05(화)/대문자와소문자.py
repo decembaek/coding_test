f8c44e2154323546a77d498f04e7d0129383866e
yeah k <yeahyep04@gmail.com>
2024-03-05 17:08:12 +0900

    2024-03-05

---
+++ yeah/24.03.05(화)/대문자와소문자.py
@@ -0,0 +1,25 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/120893
+
+문제 설명
+문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ my_string의 길이 ≤ 1,000
+my_string은 영어 대문자와 소문자로만 구성되어 있습니다.
+입출력 예
+my_string	result
+"cccCCC"	"CCCccc"
+"abCdEfghIJ"	"ABcDeFGHij"
+입출력 예 설명
+입출력 예 #1
+
+소문자는 대문자로 대문자는 소문자로 바꾼 "CCCccc"를 return합니다.
+입출력 예 #2
+
+소문자는 대문자로 대문자는 소문자로 바꾼 "ABcDeFGHij"를 return합니다.
+
+"""
+
+def solution(my_string):
+    return my_string.swapcase()
