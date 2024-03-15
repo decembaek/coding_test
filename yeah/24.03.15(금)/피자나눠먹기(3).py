d40e5dc2d178f28dfb07088b7dd904fe660a32ba
yeah k <yeahyep04@gmail.com>
2024-03-15 13:42:18 +0900

    2024-03-15

---
+++ yeah/24.03.15(금)/피자나눠먹기(3).py
@@ -0,0 +1,46 @@
+"""
+
+https://school.programmers.co.kr/learn/courses/30/lessons/120816
+문제 설명
+머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다. 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때, n명의 사람이 최소 한 조각 이상 피자를 먹으려면 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+2 ≤ slice ≤ 10
+1 ≤ n ≤ 100
+입출력 예
+slice	n	result
+7	10	2
+4	12	3
+입출력 예 설명
+입출력 예 #1
+
+10명이 7조각으로 자른 피자를 한 조각 이상씩 먹으려면 최소 2판을 시켜야 합니다.
+입출력 예 #2
+
+12명이 4조각으로 자른 피자를 한 조각 이상씩 먹으려면 최소 3판을 시켜야 합니다.
+
+
+
+"""
+# 첫풀이 > 몇개 통과실패
+def solution(slice, n):
+    answer = 0
+    if slice > n:
+        if slice % n == 0:
+            answer = slice//n 
+        else:
+            answer = slice//n +1
+    else:
+        if n % slice == 0:
+            answer = n//slice
+        else:
+            answer = n//slice +1  
+    return answer
+
+
+# 고친
+def solution(slice, n):
+    if slice >= n:  # 피자 조각 수가 사람의 수보다 많거나 같을 경우
+        return 1
+    else:           # 피자 조각 수가 사람의 수보다 작을 경우
+        return (n + slice - 1) // slice
