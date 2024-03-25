0ba8b160416a53ca53cc190ab7fe8f03cbce9556
tmdrbpp123 <tmdrbpp123@gbit.kr>
2024-03-26 08:36:17 +0900

    decembaek 03 26

---
+++ decembaek/2024_03/2024_03_26/A강조하기.py
@@ -0,0 +1,16 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/181874
+프로그래머스 Lv.0
+
+제목 : A 강조하기
+"""
+
+
+# decembaek 풀이
+def solution(myString):
+    return myString.lower().replace("a", "A")
+
+
+# 다른사람
+def solution(myString):
+    return myString.lower().replace("a", "A")
---
+++ decembaek/2024_03/2024_03_26/특정한_문자를_대문자로_바꾸기.py
@@ -0,0 +1,16 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/181873
+프로그래머스 Lv.0
+
+제목 : 특정한 문자를 대문자로 바꾸기
+"""
+
+
+# decembaek 풀이
+def solution(my_string, alp):
+    return my_string.replace(alp, alp.upper())
+
+
+# 다른사람
+def solution(my_string, alp):
+    return my_string.replace(alp, alp.upper())
